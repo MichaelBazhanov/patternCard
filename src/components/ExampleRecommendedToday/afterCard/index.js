880dc8d618d5105d5814c7ac396e9e521e90c776
michael bazhanov <michaelbazhanov33@gmail.com>
2022-08-11 17:58:26 +0300

    fix component Recimendated afterCard

--- src/components/ExampleRecommendedToday/afterCard/index.js
+++ src/components/ExampleRecommendedToday/afterCard/index.js
@@ -27,64 +27,74 @@ const AfterCard = () => {
     >
       <h2 className="font-extrabold text-4xl text-center mt-4">After Card</h2>
 
-      <article className="bg-stone-300 rounded-md
+      <article className="z-0 bg-stone-300 rounded-md
       w-[300px] h-[356px] p-[10px] xl:w-[400px] xl:h-[500px] 3xl:w-[573px] 3xl:h-[500px] ">
         <div className="relative pb-[119%]">
+
+          {/* Основная ссылка по умолчанию */}
           <a
             onClick={alertFunction}
             data-link="Карточка товара (c множеством ссылок)"
             href="/following-a-link-1"
+            className="before:content-[''] before:block before:absolute before:-inset-0 before:cursor-pointer before:-z-[1]
+            hover:bg-red-500"
+            >
+            <img src={divan} alt="img" role='img' className="z-[1] absolute inset-0 w-full h-full object-center object-cover"/>
+          </a>
+
+          {/* Общее позиционирование внутри блока */}
+          <div
             className="absolute inset-0 flex flex-col text-white
             p-[20px] pt-[15px] xl:p-[25px] xl:pb-[30px] 3xl:p-[25px] 3xl:pb-[40px]
             "
           >
-            <img src={divan} alt="img" role='img' className="z-[1] absolute inset-0 w-full h-full object-center object-cover"/>
 
             <div className="relative w-full h-full inset-0">
 
               <PositionInCard top='0px' left='0px'>
                 <a
-                onClick={alertFunction}
-                data-link="Карточка товара (c множеством ссылок)"
-                href="/following-a-link-4"
-                className="z-[2] relative whitespace-nowrap font-medium text-sm xl:text-lg 3xl:text-2xl">1 — 9 августа</a>
+                  onClick={alertFunction}
+                  data-link="Карточка товара (c множеством ссылок)"
+                  href="/following-a-link-4"
+                  className="z-[1] relative whitespace-nowrap font-medium text-sm xl:text-lg 3xl:text-2xl">1 — 9 августа</a>
               </PositionInCard>
 
               <PositionInCard bottom='0px' left='0px' right='0px'>
                 <a onClick={alertFunction}
-                    data-link="Карточка товара (c множеством ссылок)"
-                    href="/following-a-link-3">
+                  data-link="Карточка товара (c множеством ссылок)"
+                  href="/following-a-link-3">
 
-                <div className="z-[2] relative font-medium text-xl 3xl:text-3xl">Звездный август</div>
-                <div className="z-[2] relative mt-[5px] xl:mt-[10px] 3xl:mt-[15px] text-sm xl:text-lg 3xl:text-2xl">
-                  Погружаемся в комфорт
-                  <br />
-                  со скидками до -50%
-                </div>
+                  <div className="z-[1] relative font-medium text-xl 3xl:text-3xl">Звездный август</div>
+                  <div className="z-[1] relative mt-[5px] xl:mt-[10px] 3xl:mt-[15px] text-sm xl:text-lg 3xl:text-2xl">
+                    Погружаемся в комфорт
+                    <br />
+                    со скидками до -50%
+                  </div>
                 </a>
               </PositionInCard>
 
               <PositionInCard top='0px' right='0px'>
-                <div className="z-[2] relative w-[66px] h-[29px] flex justify-center items-center group">
+                <a className="z-[1] relative w-[66px] h-[29px] flex justify-center items-center group"
+                  onClick={alertFunction}
+                  data-link="Карточка товара (c множеством ссылок)"
+                  href="/following-a-link-2"
+                >
                   <Svg
                     src={label}
                     className='absolute inset-0 text-fuchsia-400 hover:text-fuchsia-700 transition-colors'
                     adoptive
-                    />
+                  />
 
-                    <a
-                    onClick={alertFunction}
-                    data-link="Карточка товара (c множеством ссылок)"
-                    href="/following-a-link-2"
-                    className="z-[2] relative text-black px-[10px]_ py-[5px]_ ml-auto mr-2 whitespace-nowrap font-medium text-sm xl:text-lg 3xl:text-2xl
-                    pointer-events-none group-hover:text-white transition-colors
-                    ">−50%</a>
-                </div>
+                  <div
+                  className="z-[1] relative text-black px-[10px]_ py-[5px]_ ml-auto mr-2 whitespace-nowrap font-medium text-sm xl:text-lg 3xl:text-2xl
+                  pointer-events-none group-hover:text-white transition-colors
+                  ">−50%</div>
+                </a>
               </PositionInCard>
 
             </div>
 
-          </a>
+          </div>
         </div>
       </article>
 
